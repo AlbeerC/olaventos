40cd6f717cb58bbeeb8b787f174762e89509c62c
Alberto Caminos <albeerc@gmail.com>
2025-12-01 14:07:20 -0300

    agregué link para ir al detalle desde la card de eventos en el panel de creador

--- frontend-react/src/components/PanelCreadorCarta/PanelCreadorCarta.jsx
+++ frontend-react/src/components/PanelCreadorCarta/PanelCreadorCarta.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { formatearFecha } from "../../utils/formatearFecha";
 import ModalConfirmacion from "../ModalConfirmacion/ModalConfirmacion";
 import { toast } from "react-toastify";
+import { Link } from "react-router";
 
 function PanelCreadorCarta({ evento, eliminarEvento, actualizarEvento }) {
   const [modalAbierto, setModalAbierto] = useState(false);
@@ -25,12 +26,14 @@ function PanelCreadorCarta({ evento, eliminarEvento, actualizarEvento }) {
   };
 
   return (
-    <div className="evento">
+    <div className="evento-creador">
       <div class="arriba">
         <h3>{evento.titulo}</h3>
         <span>Publicado</span>
       </div>
-      <img src={evento.imagen} alt={evento.titulo} />
+      <Link to={`/detalle/${evento.id}`}>
+        <img src={evento.imagen} alt={evento.titulo} />
+      </Link>
       <p class="fecha">{formatearFecha(evento.fecha)}</p>
       <p>
         {evento.lugar} - {evento.direccion}
